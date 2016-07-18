10e0e85bda0f587e190bb3a67303e6e51daa4636
Scott Levy <scottlevy89@gmail.com>
2016-07-18 14:59:35 -0400

    Add dynamic pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,14 +2,10 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
-
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Rental Car App"
   end
 
   # test "should get about" do
